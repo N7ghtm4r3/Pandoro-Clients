4d5514b7a0a77a13af8f76fb2cc73a12bcd33247
N7ghtm4r3 <maurizio.manuel2003@gmail.com>
2025-08-30 15:32:35 +0200

    Integrated fragmentCompose for Android and Generated Dokka's docu!

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -71,6 +71,7 @@ kotlin {
         androidMain.dependencies {
             implementation(compose.preview)
             implementation(libs.androidx.activity.compose)
+            implementation(libs.androidx.fragment.compose)
             implementation(libs.ktor.client.okhttp)
             implementation(libs.androidx.startup.runtime)
             implementation(libs.app.update)
